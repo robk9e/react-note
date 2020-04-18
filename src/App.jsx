2c5b7f697738f90b27043ac38c6875151b85af71
Carvalho <robsoncj@a483e754e123.ant.amazon.com>
2020-04-18 04:22:37 +0100

    LESSON-14-EXECUTING-GRAPHQL-MUTATION

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 
+/* Add note module */
+import { API, graphqlOperation } from "aws-amplify";
+
+/* Import create note mutation file */
+import { createNote } from "./graphql/mutations";
+
 import { withAuthenticator } from "aws-amplify-react";
 
 class App extends Component {
   state = {
-    notes: [
-      {
-        id: 1,
-        note: "Hello World",
-      },
-    ],
+    note: "",
+    notes: [],
+  };
+  handleChangeNote = (event) => {
+    this.setState({ note: event.target.value });
+  };
+
+  handleAddNote = (event) => {
+    event.preventDefault();
+    const { note } = this.state;
+    const input = {
+      note,
+    };
+    API.graphql(graphqlOperation(createNote, { input }));
   };
 
   render() {
@@ -18,8 +32,13 @@ class App extends Component {
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-1">Amplify Notetaker</h1>
         {/* Note Form */}
-        <form className="mb3">
-          <input type="text" className="pa2 f4" placeholder="write your note" />
+        <form className="mb3" onSubmit={this.handleAddNote}>
+          <input
+            type="text"
+            className="pa2 f4"
+            placeholder="write your note"
+            onChange={this.handleChangeNote}
+          />
           <button className="pa2 f4" type="submit">
             Add Note
           </button>
